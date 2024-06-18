681ba4610e9bf25c3cec2a867217964a6b13107c
Ei Thinzar Phyo <eithinzarphyo430@gmail.com>
2024-06-18 17:06:41 +0630

    added Swiper function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { Link } from 'react-scroll';
+//import { Link } from 'react-scroll';
 import { BiChevronDown, BiRestaurant } from 'react-icons/bi';
 import Button from '../layouts/Button';
 import { AiOutlineClose } from 'react-icons/ai';
 import { AiOutlineMenuUnfold } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import HowTo from './pages/HowTo';
+
+
 
 const Navbar = () => {
     const [menu, setMenu] = useState(false);
@@ -13,6 +17,7 @@ const Navbar = () => {
     const closeMenu = () => {
         setMenu(false);
     };
+    
     return (
         <div className='fixed w-full'>
             <div>
@@ -21,39 +26,40 @@ const Navbar = () => {
                         <span>
                             <BiRestaurant size={32} />
                         </span>
-                        <h1 className='text-xl font-semibold'>FoodieWeb</h1>
+                        <h1 className='text-xl font-semibold'>BurmeseCuisine</h1>
                     </div>
                     <nav className='hidden md:flex flex-row item-center text-lg font-medium gap-8'>
-                        <Link to='home' spy={true} smooth={true} duration={500}
+                        <Link to='/Home' spy={true} smooth={true} duration={500}
                             className='hover:text-brightColor transition-all cursor-pointer'>Home</Link>
                         <div className='relative group'>
                             <div className='flex items-center gap-1'>
                                 <Link to='dishes' spy={true} smooth={true} duration={500}
-                                    className='hover:text-brightColor transition-all cursor-pointer'>Dishes</Link>
+                                    className='hover:text-brightColor transition-all cursor-pointer'>Categories</Link>
                                 <BiChevronDown className='cursor-pointer' size={25} />
                             </div>
                             <ul className='absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5'>
                                 <li>
                                     <Link to='dishes' spy={true} smooth={true} duration={500}
-                                        className='hover:text-brightColor transition-all cursor-pointer'>Spicy</Link>
+                                        className='hover:text-brightColor transition-all cursor-pointer'>Dishes</Link>
                                 </li>
                                 <li> <Link to='dishes' spy={true} smooth={true} duration={500}
-                                    className='hover:text-brightColor transition-all cursor-pointer'>Tasty</Link>
+                                    className='hover:text-brightColor transition-all cursor-pointer'>Salad</Link>
                                 </li>
                                 <li><Link to='dishes' spy={true} smooth={true} duration={500}
-                                    className='hover:text-brightColor transition-all cursor-pointer'>Delicious</Link>
+                                    className='hover:text-brightColor transition-all cursor-pointer'>Dessert</Link>
                                 </li>
-                                <li><Link to='dishes' spy={true} smooth={true} duration={500}
+                                {/* <li><Link to='dishes' spy={true} smooth={true} duration={500}
                                     className='hover:text-brightColor transition-all cursor-pointer'>Crispy</Link>
-                                </li>
+                                </li> */}
                             </ul>
                         </div>
-                        <Link to='about' spy={true} smooth={true} duration={500}
-                            className='hover:text-brightColor transition-all cursor-pointer'>About</Link>
-                        <Link to='menu' spy={true} smooth={true} duration={500}
-                            className='hover:text-brightColor transition-all cursor-pointer'>Menu</Link>
-                        <Link to='reviews' spy={true} smooth={true} duration={500}
-                            className='hover:text-brightColor transition-all cursor-pointer'>Reviews</Link>
+                        <Link to='/HowTo' spy={true} smooth={true} duration={500}
+                            className='hover:text-brightColor transition-all cursor-pointer'
+                            >How to..</Link>
+                        <Link to='Articles' spy={true} smooth={true} duration={500}
+                            className='hover:text-brightColor transition-all cursor-pointer'>Articles&Blog</Link>
+                        <Link to='Shop' spy={true} smooth={true} duration={500}
+                            className='hover:text-brightColor transition-all cursor-pointer'>Shop</Link>
                         <Button title="Login" />
                     </nav>
                     <div className="md:hidden flex items-center">
@@ -68,14 +74,15 @@ const Navbar = () => {
                     <Link to='home' spy={true} smooth={true} duration={500}
                         className='hover:text-brightColor transition-all cursor-pointer'>Home</Link>
                     <Link to='dishes' spy={true} smooth={true} duration={500}
-                        className='hover:text-brightColor transition-all cursor-pointer'>Dishes</Link>
+                        className='hover:text-brightColor transition-all cursor-pointer'>Categories</Link>
                     <Link to='about' spy={true} smooth={true} duration={500}
-                        className='hover:text-brightColor transition-all cursor-pointer'>About</Link>
+                        className='hover:text-brightColor transition-all cursor-pointer'>How to..</Link>
                     <Link to='menu' spy={true} smooth={true} duration={500}
-                        className='hover:text-brightColor transition-all cursor-pointer'>Menu</Link>
+                        className='hover:text-brightColor transition-all cursor-pointer'>Articles&Blog</Link>
                     <Link to='reviews' spy={true} smooth={true} duration={500}
-                        className='hover:text-brightColor transition-all cursor-pointer'>Reviews</Link>
+                        className='hover:text-brightColor transition-all cursor-pointer'>Shop</Link>
                     <Button title="Login" />
+                    
                 </div>
             </div>
         </div>
---
+++ src/pages/howTo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HowTo = () => {
+    return(
+        <div>
+            <h1>Hello everyone</h1>
+        </div>
+    )
+}
+export default HowTo;
