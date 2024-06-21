04ad1c3589a5588d139bd86277a45814dc89f2ff
Nwe <user@usernoMacBook-Pro.local>
2024-06-21 08:58:05 +0630

    Updated array function in slider and some responsive ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,117 +1,122 @@
 import React from "react";
 import Button from "../layouts/Button";
-import { Swiper,SwiperSlide } from "swiper/react";
-import'swiper/css';
-import'swiper/css/effect-coverflow';
-import'swiper/css/pagination';
-import'swiper/css/navigation';
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import homeBg from "../assets/homeBg.jpg"
-import {EffectCoverflow,Pagination,Navigation} from 'swiper/modules'
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules'
+import arr from "../../recipie.json"
+import { Container,Grid } from "@mui/material";
+
 
 const Home = () => {
-    return (
-        <div>
-        <div
-  className="min-h-screen flex flex-row justify-between 
+
+  const data = arr
+
+  return (
+    <div>
+      <div
+        className="min-h-screen flex flex-row justify-between 
         lg:px-32 px-5 bg-cover bg-no-repeat"
-  style={{
-    backgroundImage: `url(${homeBg})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  }}
->
-<div className="w-full lg:w-2/3 space-y-5 textPosition">
-  <h1 className="text-backgroundColor font-semibold text-5xl slidein mt-12">Stunning Food & Recipes</h1>
-  <h2 className="text-backgroundColor font-semibold text-2xl slideout mt-12">Fresh & Fabulous Recipes for Food Lovers</h2>
-  <p className="text-backgroundColor slideTop">"Delicious Recipes for Every Occasion"</p>
-</div>
+        style={{
+          backgroundImage: `url(${homeBg})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div className="w-full lg:w-2/3 space-y-5 textPosition">
+          <h1 className="text-backgroundColor font-semibold text-5xl slidein mt-12">Stunning Food & Recipes</h1>
+          <h2 className="text-backgroundColor font-semibold text-2xl slideout mt-12">Fresh & Fabulous Recipes for Food Lovers</h2>
+          <p className="text-backgroundColor slideTop">"Delicious Recipes for Every Occasion"</p>
+        </div>
 
-<div>
-<div className="flex flex-row " style={{marginTop:80}}>
-<img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsVW9jhOkMYZJbibfxQEfPg6ZDKKY7pGzkQg&s"
-        alt="A visually appealing image that complements your content"
-        className="w-full h-auto rounded shadow-md slidein"
-        style={{width:200,height:110,borderRadius:30,marginTop:60}}
-      />
-      <img
-        src="https://res.cloudinary.com/rainforest-cruises/images/c_fill,g_auto/f_auto,q_auto/w_1120,h_732,c_fill,g_auto/v1620075704/Myanmar-Traditional-Dishes-To-Try-Mohinga/Myanmar-Traditional-Dishes-To-Try-Mohinga-1120x732.jpg"
-        alt="A visually appealing image that complements your content"
-        className="w-full h-auto rounded shadow-md slideout"
-        style={{width:180,height:150,borderTopLeftRadius:30,borderBottomRightRadius:30,marginLeft:20}}
-      />
-      
-</div>
-<div className="flex flex-row " >
+   
 
-<img
-        src="https://www.gomyanmartours.com/wp-content/uploads/2014/11/Shan-%E2%80%98tofu%E2%80%99-noodles-delicious-Myanmar-traditional-food.jpg"
-        alt="A visually appealing image that complements your content"
-        className="w-full h-auto rounded shadow-md slideTop"
-        style={{width:80,height:180,marginTop:25,borderTopLeftRadius:20,borderBottomRightRadius:20}}
-      />
-      <img
-        src="https://www.luckytreasuretravels.com/media/cache/a0/86/a086328fa9a43bf5467dfa51a6662024.jpg"
-        alt="A visually appealing image that complements your content"
-        className="w-full h-auto rounded shadow-md slideout"
-        style={{width:250,height:150,borderRadius:20,marginLeft:20,marginTop:20}}
-      />
+        <div>
+          <div className="flex flex-row " style={{ marginTop: 80 }}>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsVW9jhOkMYZJbibfxQEfPg6ZDKKY7pGzkQg&s"
+              alt="A visually appealing image that complements your content"
+              className="w-full h-auto rounded shadow-md slidein"
+              style={{ width: 200, height: 110, borderRadius: 30, marginTop: 60 }}
+            />
+            <img
+              src="https://res.cloudinary.com/rainforest-cruises/images/c_fill,g_auto/f_auto,q_auto/w_1120,h_732,c_fill,g_auto/v1620075704/Myanmar-Traditional-Dishes-To-Try-Mohinga/Myanmar-Traditional-Dishes-To-Try-Mohinga-1120x732.jpg"
+              alt="A visually appealing image that complements your content"
+              className="w-full h-auto rounded shadow-md slideout"
+              style={{ width: 180, height: 150, borderTopLeftRadius: 30, borderBottomRightRadius: 30, marginLeft: 20 }}
+            />
+
+          </div>
+          <div className="flex flex-row " >
+
+            <img
+              src="https://www.gomyanmartours.com/wp-content/uploads/2014/11/Shan-%E2%80%98tofu%E2%80%99-noodles-delicious-Myanmar-traditional-food.jpg"
+              alt="A visually appealing image that complements your content"
+              className="w-full h-auto rounded shadow-md slideTop"
+              style={{ width: 80, height: 180, marginTop: 25, borderTopLeftRadius: 20, borderBottomRightRadius: 20 }}
+            />
+            <img
+              src="https://www.luckytreasuretravels.com/media/cache/a0/86/a086328fa9a43bf5467dfa51a6662024.jpg"
+              alt="A visually appealing image that complements your content"
+              className="w-full h-auto rounded shadow-md slideout"
+              style={{ width: 250, height: 150, borderRadius: 20, marginLeft: 20, marginTop: 20 }}
+            />
 
 
-</div>
-</div>
-</div>
-
-        <div className='container' style={{backgroundColor:"#f5f5fa"}}>
-          <Swiper effect={'coverflow'} grabCursor={true} centeredSlides={true} loop={true} slidesPerView={'auto'} coverflowEffect={{
-            rotate:0,
-            stretch:0,
-            depth:100,
-            modifier:2.5,
-
-          }}
-          pagination={{el:'.swiper-pagination',clickable:true}}
-          navigation={{
-            nextEl:'.swiper-button-next',
-            prevEl:'.swiper-button-prev',
-            clickable:true,
-          }}
-          modules={[EffectCoverflow,Pagination,Navigation]}
-          className='swiper_container'
-          >
-            <SwiperSlide>
-              <img src="https://www.luckytreasuretravels.com/media/img/vt/e775afebea718e4f74cc40ce2b271bed08e7001.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://static.wixstatic.com/media/574c0d_67a97ef27708476b89d113e544a1a96a~mv2.jpg/v1/fill/w_1000,h_666,al_c,q_90,usm_0.66_1.00_0.01/574c0d_67a97ef27708476b89d113e544a1a96a~mv2.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://www.luckytreasuretravels.com/media/img/vt/e775afebea718e4f74cc40ce2b271bed08e7001.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://www.luckytreasuretravels.com/media/img/vt/e775afebea718e4f74cc40ce2b271bed08e7001.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://www.luckytreasuretravels.com/media/img/vt/e775afebea718e4f74cc40ce2b271bed08e7001.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://www.luckytreasuretravels.com/media/img/vt/e775afebea718e4f74cc40ce2b271bed08e7001.jpg" alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src="https://www.luckytreasuretravels.com/media/img/vt/e775afebea718e4f74cc40ce2b271bed08e7001.jpg" alt="" />
-            </SwiperSlide>
-            <div className="slider-controler">
-              <div className="swiper-button-prev slider-arrow">
-                <ion-icon name="arrow-back-outline"></ion-icon>
-              </div>
-              <div className="swiper-button-next slider-arrow">
-                <ion-icon name="arrow-forward-outline"></ion-icon>
-              </div>
-              <div className="swiper-pagination"></div>
-            </div>
-          </Swiper>
           </div>
         </div>
-    )
+      </div>
+
+      <div className='container' style={{ backgroundColor: "#f5f5fa" }}>
+        <Swiper effect={'coverflow'} grabCursor={true} centeredSlides={true} loop={true} slidesPerView={'auto'} coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+
+        }}
+          pagination={{ el: '.swiper-pagination', clickable: true }}
+          navigation={{
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            clickable: true,
+          }}
+          modules={[EffectCoverflow, Pagination, Navigation]}
+          className='swiper_container'
+        >
+
+
+          {
+            data.Dessert.map((item,index) => 
+              <SwiperSlide>
+                <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={120} sm={100} md={60} >
+        <img src={item.url} alt="" />
+
+            </Grid>
+        
+      </Grid>
+    </Container>          </SwiperSlide>
+            )
+          }
+
+         
+          <div className="slider-controler">
+            <div className="swiper-button-prev slider-arrow">
+              <ion-icon name="arrow-back-outline"></ion-icon>
+            </div>
+            <div className="swiper-button-next slider-arrow">
+              <ion-icon name="arrow-forward-outline"></ion-icon>
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
+        </Swiper>
+      </div>
+    </div>
+  )
 }
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react'
-//import { Link } from 'react-scroll';
 import { BiChevronDown, BiRestaurant } from 'react-icons/bi';
 import Button from '../layouts/Button';
-import { AiOutlineClose } from 'react-icons/ai';
-import { AiOutlineMenuUnfold } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
-import HowTo from './pages/HowTo';
+import { Link,  useLocation } from 'react-router-dom';
+import { Stack } from '@mui/material';
+
 const Navbar = () => {
     const [menu, setMenu] = useState(false);
     const handleChange = () => {
@@ -14,59 +12,123 @@ const Navbar = () => {
     const closeMenu = () => {
         setMenu(false);
     };
+
+    const location = useLocation();
+
+    const getLinkClassName = (path) => {
+        return location.pathname === path ? 'text-activeColor' : 'hover:text-brightColor';
+    };
     return (
-        <div className='fixed w-full'>
-            <div>
-                <div className='flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgba(0,0,0,0.2)]'
-                style={{backgroundColor:"#f5f5fa"}}>
-                    <div className='flex flex-row items-center cursor-pointer'>
-                        <span>
-                            <BiRestaurant size={32} />
-                        </span>
-                        <h1 className='text-xl font-semibold'>BurmeseCuisine</h1>
+        <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            p={1}
+            sx={{ position: "sticky", top: 0, backgroundColor: "#FFFFFF", boxShadow: "0px 3px 2px -2px gray", paddingLeft: 2, paddingRight: 2 }}
+            zIndex={900}
+        >
+
+
+            <div className='flex flex-row items-center cursor-pointer'>
+                <span>
+                    <BiRestaurant size={32} />
+                </span>
+                <h1 className='text-xl font-semibold'>BurmeseCuisine</h1>
+            </div>
+            <nav className='hidden md:flex flex-row items-center text-lg font-medium gap-8'>
+                <Link
+                    to='/Home'
+                    spy="true"
+                    smooth="true"
+                    duration={500}
+                    className={`${getLinkClassName('/Home')} transition-all cursor-pointer`}
+                >
+                    Home
+                </Link>
+                <div className='relative group'>
+                    <div className='flex gap-1'>
+                        <Link
+                            to='dishes'
+                            spy="true"
+                            smooth="true"
+                            duration={500}
+                            className={`${getLinkClassName('dishes')} transition-all cursor-pointer`}
+                        >
+                            Categories
+                        </Link>
                     </div>
-                    <nav className='hidden md:flex flex-row item-center text-lg font-medium gap-8'>
-                        <Link to='/Home' spy="true" smooth="true" duration={500}
-                            className='hover:text-brightColor transition-all cursor-pointer'>Home</Link>
-                        <div className='relative group'>
-                            <div className='flex items-center gap-1'>
-                                <Link to='dishes' spy="true" smooth="true" duration={500}
-                                    className='hover:text-brightColor transition-all cursor-pointer'>Categories</Link>
-                                <BiChevronDown className='cursor-pointer' size={25} />
-                            </div>
-                            <ul className='absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5'>
-                                <li>
-                                    <Link to='dishes' spy="true" smooth="true" duration={500}
-                                        className='hover:text-brightColor transition-all cursor-pointer'>Dishes</Link>
-                                </li>
-                                <li> <Link to='dishes' spy="true" smooth="true" duration={500}
-                                    className='hover:text-brightColor transition-all cursor-pointer'>Salad</Link>
-                                </li>
-                                <li><Link to='dishes' spy="true" smooth="true" duration={500}
-                                    className='hover:text-brightColor transition-all cursor-pointer'>Dessert</Link>
-                                </li>
-                                {/* <li><Link to='dishes' spy="true" smooth="true" duration={500}
-                                    className='hover:text-brightColor transition-all cursor-pointer'>Crispy</Link>
-                                </li> */}
-                            </ul>
-                        </div>
-                        <Link to='/HowTo' spy="true" smooth="true" duration={500}
-                            className='hover:text-brightColor transition-all cursor-pointer'
-                            >How to..</Link>
-                        <Link to='/articles' spy="true" smooth="true" duration={500}
-                            className='hover:text-brightColor transition-all cursor-pointer'>Articles&Blog</Link>
-                        <Link to='Shop' spy="true" smooth="true" duration={500}
-                            className='hover:text-brightColor transition-all cursor-pointer'>Shop</Link>
-                        <Button title="Login"/>
-                    </nav>
-                    <div className="md:hidden flex items-center">
+                    <ul className='absolute hidden space-y-2 group-hover:block bg-white border border-gray-300 rounded-lg p-5'>
+                        <li>
+                            <Link
+                                to='dishes'
+                                spy="true"
+                                smooth="true"
+                                duration={500}
+                                className={`${getLinkClassName('dishes')} transition-all cursor-pointer`}
+                            >
+                                Dishes
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to='dishes'
+                                spy="true"
+                                smooth="true"
+                                duration={500}
+                                className={`${getLinkClassName('dishes')} transition-all cursor-pointer`}
+                            >
+                                Salad
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to='dishes'
+                                spy="true"
+                                smooth="true"
+                                duration={500}
+                                className={`${getLinkClassName('dishes')} transition-all cursor-pointer`}
+                            >
+                                Dessert
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <Link
+                    to='/how-to'
+                    spy="true"
+                    smooth="true"
+                    duration={500}
+                    className={`${getLinkClassName('/how-to')} transition-all cursor-pointer`}
+                >
+                    How to..
+                </Link>
+                <Link
+                    to='/articles'
+                    spy="true"
+                    smooth="true"
+                    duration={500}
+                    className={`${getLinkClassName('/articles')} transition-all cursor-pointer`}
+                >
+                    Articles&Blog
+                </Link>
+                <Link
+                    to='/Shop'
+                    spy="true"
+                    smooth="true"
+                    duration={500}
+                    className={`${getLinkClassName('/Shop')} transition-all cursor-pointer`}
+                >
+                    Shop
+                </Link>
+                <Button title="Login" />
+            </nav>
+            {/* <div className="md:hidden flex items-center">
                         {menu ?
                             (<AiOutlineClose size={25} onClick={handleChange} />) : (
                                 <AiOutlineMenuUnfold size={25} onClick={handleChange}/>
                             )
                         }
                     </div>
-                </div>
                 <div className={`${menu ? "translate-x-0" : "-translatex-x-full"} lg:hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}>
                     <Link to='home' spy={true} smooth={true} duration={500}
                         className='hover:text-brightColor transition-all cursor-pointer'>Home</Link>
@@ -80,9 +142,9 @@ const Navbar = () => {
                         className='hover:text-brightColor transition-all cursor-pointer'>Shop</Link>
                     <Button title="Login" />
                     
-                </div>
-            </div>
-        </div>
+                </div> */}
+
+        </Stack>
     )
 }
 export default Navbar;
